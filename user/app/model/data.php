7c680d6f538574f95211dc4f78679b159086f404
congdtps <congdtps40376@gmail.com>
2024-10-30 17:44:13 +0700

    sourch code cho ae làm

---
+++ user/app/model/data.php
@@ -0,0 +1,7 @@
+<?php
+    ehco (`
+        Tình yêu anh dành cho em như tấm gương vậy, cho dù em có đập vỡ nó thành hàng trăm mảnh đi chăng nữa. 
+        Thì em hãy nhìn em hàng trăm mảnh ấy vẫn là em.
+    `)
+
+?>
